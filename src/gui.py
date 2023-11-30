bc08044a550da4816d1a9e1e0e4c6bfb231fb888
TootiFruti <fake@mail.com>
2023-11-30 19:47:23 +0530

    WallStall

---
+++ src/gui.py
@@ -0,0 +1,143 @@
+from PyQt5.QtWidgets import QMainWindow, QApplication
+from PyQt5.QtGui import QPixmap, QImage
+from PyQt5 import uic
+import requests
+from src.wallpaper_fetcher import wallFetcher
+
+
+class MainWindow(QMainWindow):
+    def __init__(self, daddypath, wallheavenApi):
+        super(QMainWindow, self).__init__()
+        uic.loadUi(f"{daddypath}/src/MainWindow.ui", self)
+        self.show()
+
+        self.Image.setScaledContents(True)
+
+        imagedata = [" "]
+        args = []
+        currentImage = [0]
+        # self.setImage(imagedata[currentImage[0]][1], currentImage)
+
+        self.leftButton.clicked.connect(
+            lambda: self.onLeftClick(currentImage, imagedata))
+        self.rightButton.clicked.connect(
+            lambda: self.onRightClick(currentImage, imagedata, wallheavenApi))
+
+        self.searchBarBtn.clicked.connect(
+            lambda: self.onSearchBarBtnClicked(currentImage, imagedata, wallheavenApi))
+
+        self.saveTheImageBtn.clicked.connect(
+            lambda: self.saveImage(currentImage, imagedata, "test_img.png"))
+
+    def saveImage(self, currentImage, imagedata, imgPath):
+        imagedata = eval(imagedata[0])
+        url = imagedata[currentImage[0]][1]
+        with open(imgPath, "wb") as f:
+            f.write(requests.get(url).content)
+            print(f"saved the image at {imgPath}")
+            f.close()
+
+    def setImage(self, url, currentImage):
+        img = QImage()
+        img.loadFromData(requests.get(url).content)
+        img = QPixmap(img)
+        self.Image.setPixmap(img)
+        print(f"current image = {url} currentImage={currentImage[0]}")
+
+    def getImageData(self, args, wallheavenApi):
+        ImagesData = wallFetcher(wallheavenApi, args)
+        return ImagesData
+
+    def onLeftClick(self, currentImage, imagedata):
+        imagedata = eval(imagedata[0])
+        if currentImage[0] == 0:
+            print("Reached the left end!")
+        else:
+            url = imagedata[currentImage[0]-1][1]
+            print(f"currentImage[0]-1: {currentImage[0]-1}")
+            self.setImage(url, currentImage)
+            currentImage[0] = currentImage[0] - 1
+            self.updateTextArea(
+                "wallheaven,cc", [str(imagedata)], [currentImage[0]-1])
+
+    def onRightClick(self, currentImage, imagedata, wallheavenApi):
+        imagedata = eval(imagedata[0])
+        print(f"currentImage: {currentImage[0]}")
+        url = imagedata[currentImage[0]+1][1]
+        print(f"currentImage[0]+1: {currentImage[0]+1}")
+        self.setImage(url, currentImage)
+        self.updateTextArea("wallheaven,cc", [str(imagedata)], currentImage)
+        currentImage[0] = currentImage[0] + 1
+        if currentImage[0] == 23:
+            print("Reached right end!\nLoading more data..")
+            imagedata = self.getImageData(
+                self.genArgs(wallheavenApi), wallheavenApi)
+            print("Loaded!")
+            currentImage[0] = 0
+
+    def updateTextArea(self, site, imagedata, currentImage):
+        imagedata = eval(imagedata[0])
+        temp = imagedata[currentImage[0]+1]
+        id = temp[0]
+        resolution = temp[2]
+        file_type = temp[3]
+        imageSize = temp[4]*(1/125000)
+        path = temp[1]
+        text = f"Site: {site}\nID: {id}\nResolution: {resolution}\nImage type: {file_type}\nImage size: {round(imageSize, 1)}MB\nImage link: {path}"
+        self.TextArea.setText(text)
+
+    def genArgs(self, wallheavenApi):
+        purity = ""
+        categories = ""
+        if self.sfw.isChecked():
+            purity += "100/"
+        if self.NSFW.isChecked():
+            if len(wallheavenApi.replace(" ", "").lower()) != 0:
+                purity += "111/"
+            else:
+                print("COULDNT FIND AN API KEY, DROPPED NSFW CODE")
+
+        if self.sketchy.isChecked():
+            if len(wallheavenApi.replace(" ", "").lower()) != 0:
+                purity += "110/"
+            else:
+                print("COULDNT FIND AN API KEY, DROPPED sketch CODE")
+
+        if self.general.isChecked():
+            categories += "100/"
+        if self.anime.isChecked():
+            categories += "101/"
+        if self.people.isChecked():
+            categories += "111/"
+
+        sorting = str(self.comboBox.currentText())
+        t = {
+            "tagname": self.searchBar.text(),
+            "purity": purity,
+            "categories": categories,
+            "sorting": sorting
+        }
+        return t
+
+    def onSearchBarBtnClicked(self, currentImage, imagedata, wallheavenApi):
+        args = self.genArgs(wallheavenApi)
+        print(f"args : {args}")
+        imagedata[0] = str(self.getImageData(args, wallheavenApi))
+        currentImage[0] = 0
+        imagedata = eval(imagedata[0])
+        if len(imagedata) == 0:
+            print(f"Got nothing. {imagedata}")
+        else:
+            self.setImage(imagedata[currentImage[0]][1], currentImage)
+            self.updateTextArea(
+                "wallheaven,cc", [str(imagedata)], [-1])
+
+
+def main(daddypath, wallheavenApi):
+    app = QApplication([])
+    window = MainWindow(daddypath, wallheavenApi)
+    app.exec_()
+
+
+if __name__ == "__main__":
+    main()
---
+++ src/wallpaper_fetcher.py
@@ -0,0 +1,65 @@
+import json
+import requests
+
+# default args
+default_args = {
+    "tagname": "",
+    "categories": 100,
+    "purity": 100,
+    "sorting": "random",
+    "atleast": "",
+    "ratio": "",
+    "pages": 1
+}
+
+
+def wallFetcher(wallheaven_api, args):
+    global default_args
+    if wallheaven_api == "":
+        isWallheavenApi = False
+        print("NO API")
+        BASE_URL = "https://wallhaven.cc/api/v1/search?"
+    else:
+        isWallheavenApi = True
+        BASE_URL = f"https://wallhaven.cc/api/v1/search?apikey={wallheaven_api}"
+
+    try:
+        tagname = args["tagname"]
+    except KeyError:
+        tagname = default_args["tagname"]
+    tagname.replace(" ", "+")
+    try:
+        categories = args["categories"]
+    except KeyError:
+        categories = default_args["categories"]
+    try:
+        purity = args["purity"]
+    except KeyError:
+        purity = default_args["purity"]
+    try:
+        sorting = args["sorting"]
+    except KeyError:
+        sorting = default_args["sorting"]
+    try:
+        atleast = args["atleast"]
+    except KeyError:
+        atleast = default_args["atleast"]
+    try:
+        ratio = args["ratio"]
+    except KeyError:
+        ratio = default_args["ratio"]
+    try:
+        pages = args["pages"]
+    except KeyError:
+        pages = default_args["pages"]
+    query_url = f"{BASE_URL}&q={tagname}&categories={categories}&purity={purity}&sorting={sorting}&atleast={atleast}&ratio={ratio}&pages={pages}"
+    print(query_url)
+    data = requests.get(query_url)
+    data = data.json()["data"]
+    # print(json.dumps(data, indent=2))
+    data_to_return = {}
+    for i in range(len(data)):
+        t = [data[i]["id"], data[i]["path"], data[i]["resolution"],
+             data[i]["file_type"], data[i]["file_size"]]
+        data_to_return[i] = t
+    return data_to_return
